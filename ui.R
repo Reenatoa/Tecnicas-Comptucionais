ac78fd93930abf71252c823ae944ec70c61fd81e
Reenatoa <reenatoa@yahoo.com>
2016-07-04 10:53:04 -0300

    segundo commit

--- ui.R
+++ ui.R
@@ -16,12 +16,11 @@ shinyUI(navbarPage("Estimação de parâmetros",
                             
                             
                             fluidRow(column(3,selectInput("select", label = h3("Escolha uma Distribuição"), 
-                                                          choices = list("Geométrica" = 1, "Normal" = 2,
-                                                                         "Beta" = 3), selected = 1))),
+                                                          choices = list("Geometrica" = "Geometrica", "Normal" = "Normal",
+                                                                         "Beta" = "Beta"), selected = "Beta"))),
                             
                             
-                            
-                            
+                            conditionalPanel(condition="input.select=='Geometrica'",
                             
                             fluidRow(
                               column(3,
@@ -47,4 +46,70 @@ shinyUI(navbarPage("Estimação de parâmetros",
                                                ))
                               
                               
-                                               ))))
+                                               )),
+                            
+                            
+                            conditionalPanel(condition="input.select=='Normal'",
+                                             
+                                             fluidRow(
+                                               column(3,
+                                                      wellPanel(
+                                                        
+                                                        helpText("Defina um tamanho da amostra e o parâmetro p"),
+                                                        numericInput("n", label="tamanho da amostra", value=10),
+                                                        #shinyalert("shinyalert2", TRUE, auto.close.after=10),
+                                                        numericInput("prob", label="valor de p entre (0,1)", value=0.5, min=0,max=1)
+                                                        
+                                                        
+                                                      )),
+                                               column(9,
+                                                      wellPanel(h3("Máxima Verossimilhança para Distribuição Geometrica"),
+                                                                withMathJax(p("Os gráficos abaixo representam a função de probabilidade e a função de log-verossimlhaça respectivamente
+                                                                              da distribuição geométrica. Sua densidade é dada por: $$P(X=x) = p(1-p)^x ,   x=0,1,...$$"),
+                                                                            p("O gráfico de log-verossimilhança é criado a partir de uma amostra aleatória de tamanho n, a linha vermelha indica
+                                                                              o valor verdadeiro do parâmetro e a linha azul indica o valor do estimador de máxima verossimilhança obtido a partir da amostra.")),
+                                                                plotOutput("asas"),
+                                                                br(),
+                                                                p("Pode-se notar que quando o tamanho da amostra aumenta o estimador de máxima verossimilhança se aproxima do valor verdadeiro do parâmetro.")
+                                                                
+                                                                            ))
+                                               
+                                               
+                                               )),
+                            
+                            
+                            
+                            conditionalPanel(condition="input.select=='Beta'",
+                                             
+                                             fluidRow(
+                                               column(3,
+                                                      wellPanel(
+                                                        
+                                                        helpText("Defina um tamanho da amostra e o parâmetro p"),
+                                                        numericInput("n", label="tamanho da amostra", value=10),
+                                                        #shinyalert("shinyalert2", TRUE, auto.close.after=10),
+                                                        numericInput("prob", label="valor de p entre (0,1)", value=0.5, min=0,max=1)
+                                                        
+                                                        
+                                                      )),
+                                               column(9,
+                                                      wellPanel(h3("Máxima Verossimilhança para Distribuição Geometrica"),
+                                                                withMathJax(p("Os gráficos abaixo representam a função de probabilidade e a função de log-verossimlhaça respectivamente
+                                                              da distribuição geométrica. Sua densidade é dada por: $$P(X=x) = p(1-p)^x ,   x=0,1,...$$"),
+                                                                            p("O gráfico de log-verossimilhança é criado a partir de uma amostra aleatória de tamanho n, a linha vermelha indica
+                                                             o valor verdadeiro do parâmetro e a linha azul indica o valor do estimador de máxima verossimilhança obtido a partir da amostra.")),
+                                                                plotOutput("as"),
+                                                                br(),
+                                                                p("Pode-se notar que quando o tamanho da amostra aumenta o estimador de máxima verossimilhança se aproxima do valor verdadeiro do parâmetro.")
+                                                                
+                                                      ))
+                                               
+                                               
+                                             ))
+                            
+                            
+                            
+                            
+                            
+                            
+                            )))
